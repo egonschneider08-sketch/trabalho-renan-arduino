c4c824100d788ed65c0b464bc581b745a39e54a4
MATHEUS EGON SCHNEIDER <matheus_schneider150@estudante.sesisenai.org.br>
2026-03-17 16:30:39 -0300

    V-1.1

--- main.cpp
+++ main.cpp
@@ -1,98 +1,89 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <Servo.h>
 
-#include <LiquidCrystal.h>
-#include <EEPROM.h>
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+Servo valvula;
 
-// LCD (RS, E, D4, D5, D6, D7)
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+const int potPin = A0;
+const int motorPin = 9;
+const int servoPin = 10;
 
-// Sensores de nível
-const int sensorNivel[5] = {6,7,8,9,10};
+const int led10 = 2;
+const int led30 = 3;
+const int led50 = 4;
+const int led70 = 5;
+const int led90 = 6;
 
-// Altura em cm
-const int alturaNivel[5] = {2,4,6,8,9};
+int nivel = 0;
+bool bombaLigada = false;
 
-// Sensor de vazão (potenciômetro)
-const int sensorVazao = A0;
+void setup() {
+  lcd.init();
+  lcd.backlight();
 
-// LED que representa a bomba
-const int ledSistema = 13;
+  valvula.attach(servoPin);
 
-int alturaAtual = 0;
-unsigned long consumoTotal = 0;
+  pinMode(motorPin, OUTPUT);
 
-void setup()
-{
-  lcd.begin(16,2);
+  pinMode(led10, OUTPUT);
+  pinMode(led30, OUTPUT);
+  pinMode(led50, OUTPUT);
+  pinMode(led70, OUTPUT);
+  pinMode(led90, OUTPUT);
 
-  for(int i=0;i<5;i++)
-  {
-    pinMode(sensorNivel[i], INPUT);
-  }
-
-  pinMode(ledSistema, OUTPUT);
-
-  EEPROM.get(0, consumoTotal);
-
-  lcd.print("Sistema Agua");
-  delay(2000);
-  lcd.clear();
+  Serial.begin(9600);
 }
 
-void loop()
-{
-  lerNivel();
-  controlarSistema();
-  calcularConsumo();
-  mostrarLCD();
+void loop() {
+  int leitura = analogRead(potPin);
+  nivel = map(leitura, 0, 1023, 0, 100);
+
+  if (nivel <= 10) {
+    bombaLigada = true;
+  }
+  if (nivel >= 90) {
+    bombaLigada = false;
+  }
+
+  if (bombaLigada) {
+    digitalWrite(motorPin, HIGH);
+    valvula.write(90);
+  } else {
+    digitalWrite(motorPin, LOW);
+    valvula.write(0);
+  }
+
+  digitalWrite(led10, nivel >= 10);
+  digitalWrite(led30, nivel >= 30);
+  digitalWrite(led50, nivel >= 50);
+  digitalWrite(led70, nivel >= 70);
+  digitalWrite(led90, nivel >= 90);
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Nivel: ");
+  lcd.print(nivel);
+  lcd.print("%");
+
+  lcd.setCursor(0, 1);
+  lcd.print("Bomba: ");
+  if (bombaLigada) {
+    lcd.print("Ligado");
+  } else {
+    lcd.print("Desligado");
+  }
+
+  Serial.print("Nivel: ");
+  Serial.print(nivel);
+  Serial.print("% | Bomba: ");
+
+  if (bombaLigada) {
+    Serial.println("Ligado");
+  } else {
+    Serial.println("Desligado");
+  }
 
   delay(500);
-}
-
-void lerNivel()
-{
-  for(int i=4;i>=0;i--)
-  {
-    if(digitalRead(sensorNivel[i]) == HIGH)
-    {
-      alturaAtual = alturaNivel[i];
-      break;
-    }
-  }
-}
-
-void controlarSistema()
-{
-  if(alturaAtual <= 4)
-  {
-    digitalWrite(ledSistema, HIGH); // reservatorio enchendo
-  }
-  else if(alturaAtual >= 9)
-  {
-    digitalWrite(ledSistema, LOW); // reservatorio cheio
-  }
-}
-
-void calcularConsumo()
-{
-  int leitura = analogRead(sensorVazao);
-
-  int fluxo = map(leitura,0,1023,0,10);
-
-  consumoTotal += fluxo;
-
-  EEPROM.put(0, consumoTotal);
-}
-
-void mostrarLCD()
-{
-  lcd.setCursor(0,0);
-  lcd.print("Nivel:");
-  lcd.print(alturaAtual);
-  lcd.print("cm   ");
-
-  lcd.setCursor(0,1);
-  lcd.print("Cons:");
-  lcd.print(consumoTotal);
-  lcd.print("L    ");
 }
